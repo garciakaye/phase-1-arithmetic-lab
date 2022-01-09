76ddaca3326781a94c7b16bd0ef7c6c22d8892bd
kaye garcia <kayemgarcia@gmail.com>
2022-01-09 16:25:29 -0600

    commit tree 81200a08eba342be49fd21fd1f6171f07e255b79

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = 2;
+Math.random(random > 0);
+
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+const num5 = 18
+const num6 = 19
+const num7 = 20
+const max = Math.max(num5, num6, num7)
